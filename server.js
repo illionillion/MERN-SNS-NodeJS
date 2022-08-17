c77b48eeec8f26d5728e17af0a8c1ef43e89c898
yuto ichinose <yuto.0526.sano@gmail.com>
2022-08-17 22:11:13 +0900

    dotenv入れた、MongoDBとNodeJSを接続

--- server.js
+++ server.js
@@ -2,6 +2,19 @@ import express from "express";
 import userRouter from "./routes/users.js";
 import authRouter from "./routes/auth.js";
 import postRouter from "./routes/posts.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+
+dotenv.config() // .env読み込み
+
+// console.log(process.env.MONGOURL);
+// データベース接続
+mongoose.connect(process.env.MONGOURL)
+.then(e => {
+    console.log('DBと接続中・・・');
+}).catch(err => {
+    console.log(err);
+}) 
 
 const app = express();
 
