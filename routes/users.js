81365047415ee5577348ca1564597ae142315bbc
yuto ichinose <yuto.0526.sano@gmail.com>
2022-08-25 20:27:01 +0900

    特定のユーザー情報を取得する

--- routes/users.js
+++ routes/users.js
@@ -32,10 +32,18 @@ userRouter.delete('/:id', async (req, res) => {
         return res.status(500).json('アカウントが違います。ユーザー情報の削除ができません。')
     }
 })
-    
+
 // ユーザー情報の取得
 userRouter.get('/:id', async (req, res) => {
-
+    // 本人かどうかのチェックは必要ない
+    try {
+        const user = await User.findById(req.params.id)
+        // パスワードなどの情報も見られてしまうので取り除く
+        const { password, updatedAt, ...other } = user._doc // 分割代入とスプレッド構文
+        res.status(200).json(other) // otherを返す
+    } catch (err) {
+        return res.status(500).json(err)
+    }
 })
 
 export default userRouter;
