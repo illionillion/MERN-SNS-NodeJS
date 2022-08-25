b9667ef3a955650f793d8b178ffcfb5c4377b884
yuto ichinose <yuto.0526.sano@gmail.com>
2022-08-25 20:14:03 +0900

    ユーザー情報の削除機能を作った

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ const userRouter = express.Router()
 // CRUD
 // ユーザー情報の更新 // :id MongoDBのオブジェクトID
 userRouter.put('/:id', async (req, res) => {
-    if (req.body.userId === req.params.id || req.body.isAdmin) {
+    if (req.body.userId === req.params.id || req.body.isAdmin) { // 他のユーザーが操作できないようにする
         try {
             const user = await User.findByIdAndUpdate(req.params.id, {
                 $set: req.body // 書き換え
@@ -16,11 +16,26 @@ userRouter.put('/:id', async (req, res) => {
             return res.status(500).json(err)
         }
     } else {
-        return res.status(500).json('アカウントが違います。')
+        return res.status(500).json('アカウントが違います。ユーザー情報の更新ができません。')
     }
 })
 // ユーザー情報の削除
-
+userRouter.delete('/:id', async (req, res) => {
+    if (req.body.userId === req.params.id || req.body.isAdmin) { // 他のユーザーが操作できないようにする
+        try {
+            const user = await User.findByIdAndDelete(req.params.id)
+            res.status(200).json('ユーザー情報の削除がされました。')
+        } catch (err) {
+            return res.status(500).json(err)
+        }
+    } else {
+        return res.status(500).json('アカウントが違います。ユーザー情報の削除ができません。')
+    }
+})
+    
 // ユーザー情報の取得
+userRouter.get('/:id', async (req, res) => {
+
+})
 
 export default userRouter;
