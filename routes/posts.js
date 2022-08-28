531173ae91404e88f1efdc45a830e520f958dddf
yuto ichinose <yuto.0526.sano@gmail.com>
2022-08-28 21:02:20 +0900

    投稿するAPI、MongoDB動かなくなったので今回はここまで

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,18 @@
 import express from "express";
+import Post from "../models/Post.js";
 const postRouter = express.Router()
 
-postRouter.get('/', (req, res) => {
-    res.send('posts router')
+// 投稿を作成する
+postRouter.post('/', async (req, res) => {
+    // インスタンス生成
+    const newPost = new Post(req.body)
+    try {
+        const savePost = await newPost.save()
+        return res.status(200).json(savePost)
+    } catch (err) {
+        // 方が間違ってたり、入力必須が満たせていないとエラー
+        return res.status(500).json(err)
+    }
 })
 
 export default postRouter
