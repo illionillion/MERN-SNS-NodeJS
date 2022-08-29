57519cab5b51a997c1ffd551b98d687d8cc54f72
yuto ichinose <yuto.0526.sano@gmail.com>
2022-08-29 22:46:03 +0900

    投稿削除のAPI作成

--- routes/posts.js
+++ routes/posts.js
@@ -28,7 +28,23 @@ postRouter.put('/:id', async (req, res) => { // :id = postId
         })
         return res.status(200).json('投稿編集に成功しました。') // 投稿できたらその内容を返す
     } catch (err) {
-        // 方が間違ってたり、入力必須が満たせていないとエラー
+        return res.status(403).json(err)
+    }
+})
+
+// 投稿を削除する
+postRouter.delete('/:id', async (req, res) => { // :id = postId
+    try {
+        const post = await Post.findById(req.params.id) // idでpost内容を取得
+        // bodyのユーザーIDと投稿のユーザーIDが等しければ削除できる
+        if (post.userId !== req.body.userId) {
+            return res.status(403).json('あなたは他の人の投稿を削除できません。')
+        }
+        // ここで削除
+        // await Post.findByIdAndDelete(req.params.id)
+        await post.deleteOne() // これで削除できる
+        return res.status(200).json('投稿削除に成功しました。') // 投稿できたらその内容を返す
+    } catch (err) {
         return res.status(403).json(err)
     }
 })
