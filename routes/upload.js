d724bc919080b45d7349828dc93c41b2ceaffebd
yuto ichinose <yuto.0526.sano@gmail.com>
2022-10-22 00:36:53 +0900

    画増のアップロード成功

--- routes/upload.js
+++ routes/upload.js
@@ -3,4 +3,23 @@ import multer from "multer";
 
 const uploadRouter = express.Router()
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "public/images") // この場所に画像を保存
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname) // 名前？
+    }
+})
+const upload = multer({storage})
+
+// 画像アップロード用のAPI
+uploadRouter.post("/",  upload.single("file")/* ⇦ミドルウェア */ , (req, res) => {
+    try {
+        return res.status(200).json({msg:"がぞうアップロード成功"})
+    } catch (error) {
+        console.log(error);
+    }
+})
+
 export default uploadRouter
