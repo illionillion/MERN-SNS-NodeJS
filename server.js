9abacdf4ee821a0c36863b4aa3f4c7f971bcaca5
yuto ichinose <yuto.0526.sano@gmail.com>
2022-10-23 08:42:08 +0900

    画像を返すエンドポイント

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ import postRouter from "./routes/posts.js";
 import mongoose from "mongoose";
 import dotenv from "dotenv"
 import uploadRouter from "./routes/upload.js";
+import path from 'path'
 
 dotenv.config() // .env読み込み
-
+const __dirname = new URL(import.meta.url).pathname
 // console.log(process.env.MONGOURL);
 // データベース接続
 mongoose.connect(process.env.MONGOURL)
@@ -22,6 +23,10 @@ const app = express();
 const PORT = 3001;
 
 // ミドルウェア
+
+// console.log(__dirname);
+// console.log(path.join(__dirname, '../public/images'));
+app.use('/images', express.static(path.join(__dirname, '../public/images')))
 app.use(express.json())
 app.use('/api/users', userRouter)
 app.use('/api/auth', authRouter)
